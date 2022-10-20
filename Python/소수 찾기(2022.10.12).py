b2592c6f72fdd598d14ac26dc74fb79aacc13ce9
dongwon <ogdongwon@gmail.com>
2022-10-20 16:56:44 +0900

    2022.10.20 lib

--- Python/소수 찾기(2022.10.12).py
+++ Python/소수 찾기(2022.10.12).py
@@ -1,8 +1,22 @@
-def solution(n):
-    count+=1
-    for x in range(3,n+1):
-        for y in range(2,x):
+"""def solution(n):
+    count=0
+    for x in range(2,n+1):
+        div=0
+        for y in range(2,x+1):
             if x%y==0:
-                12
+                div+=1
+        if div==1:
+            count+=1
+    return count
+""" # 시간초과 -> 에라토스테네스의 체 사용
+
+
+def solution(n):
+
+    tem=[True for i in range(n)]
+    print(tem)
+    for x in range(1,int(n**0.5)+1):
+        
     return 1
 
+print(solution(10))
