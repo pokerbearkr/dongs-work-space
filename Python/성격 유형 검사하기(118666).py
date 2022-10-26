0978f43d58fc09c7c04779d9b3413fbb00ba79f6
dongwon <ogdongwon@gmail.com>
2022-10-26 16:45:19 +0900

    2022.10.26 lib

---
+++ Python/성격 유형 검사하기(118666).py
@@ -0,0 +1,15 @@
+def solution(survey, choices):
+    sum1=0
+    sum2=0
+    sum3=0
+    sum4=0
+
+    for x in range(len(survey)):
+        if choices[x]==4:
+            1
+        elif choices[x]<4:
+            
+    answer = ''
+    return answer
+
+print(solution(["AN", "CF", "MJ", "RT", "NA"],[5, 3, 2, 7, 5]))
