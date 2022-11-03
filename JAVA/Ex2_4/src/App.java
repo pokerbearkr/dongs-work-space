d2e868dfbc1ad1b2f7630f2dbfa35fdef40a7c74
pokerbearkr <ogdongwon@gmail.com>
2022-11-03 19:45:12 +0900

    2022.11.02 home

---
+++ JAVA/Ex2_4/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int x = 5;
+        System.out.println(x);
+
+        x = 10;
+        System.out.println(x);
+        int age=15;
+        System.out.printf("age:%d",age);
+
+    }
+}
