862bc68c0b30855d4374226e4692130d0ee36045
dongwon <ogdongwon@gmail.com>
2022-11-03 19:47:24 +0900

    2022.11.03

---
+++ 2022.11.03/study/src/App.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+class App{
+    public static void main(String args[]){
+        int x = 10;
+        if(x==10)
+        {
+            System.out.printf("if 문 잘 작동중");
+        }else{
+            System.out.printf("hello world");
+        }
+
+    }
+}
