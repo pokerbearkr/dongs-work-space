4bff620f33a6b0859c0d2798d67054dcb1a6a052
programtuna <ogdongwon@gmail.com>
2023-06-29 00:58:05 +0900

    2023.06.29-2

--- prepare to boot camp/추억 점수.py
+++ prepare to boot camp/추억 점수.py
@@ -1,9 +1,12 @@
 def solution(name, yearning, photo):
-
-    
-    answer = []
-
+    answer = [0]
+    for x in range(len(photo)-1):
+        answer.append(0)
+    for x in range(len(photo)):
+        for y in name:
+            if y in photo[x]:
+                answer[x]+=yearning[name.index(y)]
 
     return answer
 
-print(["may", "kein", "kain", "radi"],[5, 10, 1, 3],[["may", "kein", "kain", "radi"],["may", "kein", "brin", "deny"], ["kon", "kain", "may", "coni"]])
+print(solution(["may", "kein", "kain", "radi"],[5, 10, 1, 3],[["may", "kein", "kain", "radi"],["may", "kein", "brin", "deny"], ["kon", "kain", "may", "coni"]]))
