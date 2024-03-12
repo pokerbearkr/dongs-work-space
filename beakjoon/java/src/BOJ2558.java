8c9225a63badde6a3af06271d3e02198c74dbe47
programtuna <ogdongwon@gmail.com>
2024-03-12 21:19:39 +0900

    Changes

---
+++ beakjoon/java/src/BOJ2558.java
@@ -0,0 +1,11 @@
+public class BOJ2558 {
+    public static void main(String[] args) throws Exception {
+        System.out.println(read() + read());
+    }
+
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+}
